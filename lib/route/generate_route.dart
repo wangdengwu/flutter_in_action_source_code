bf6f736ddcbf50a649c16d3d03f5faa4b205cd70
wangdengwu <dengwu.wang@gmail.com>
2021-08-13 23:48:13 +0800

    ListView

--- lib/route/generate_route.dart
+++ lib/route/generate_route.dart
@@ -23,6 +23,7 @@ import 'package:flutter_in_action_source_code/chapter5/decoratedbox.dart';
 import 'package:flutter_in_action_source_code/chapter5/material_scaffold.dart';
 import 'package:flutter_in_action_source_code/chapter5/padding.dart';
 import 'package:flutter_in_action_source_code/chapter5/transform.dart';
+import 'package:flutter_in_action_source_code/chapter6/listview.dart';
 import 'package:flutter_in_action_source_code/chapter6/single_child_scrollview.dart';
 
 class GenerateRoute {
@@ -58,6 +59,7 @@ class GenerateRoute {
   static const String material_scaffold_5 = "material_scaffold_5";
   static const String clip_5 = "clip_5";
   static const String single_child_scrollview_6 = "single_child_scrollview_6";
+  static const String listview_6 = "listview_6";
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -163,6 +165,10 @@ class GenerateRoute {
       case single_child_scrollview_6:
         return MaterialPageRoute(builder: (context) {
           return SingleChildScrollviewWidget();
+        });
+      case listview_6:
+        return MaterialPageRoute(builder: (context) {
+          return ListViewWidget();
         });
       default:
         return MaterialPageRoute(
