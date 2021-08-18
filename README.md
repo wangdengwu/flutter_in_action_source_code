4edb7ef6a859bcdb797eada32564a53998df0533
wangdengwu <dengwu.wang@gmail.com>
2021-08-18 13:42:34 +0800

    WebView

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # flutter实战电子书实例代码
 
 由于原电子书对应实例代码已经有些过时了，本人学习时刚好需要练习，所以新建一个实例代码库
-
+本代码对应版本:
+Flutter 2.2.3 • channel stable • https://github.com/flutter/flutter.git
+Framework • revision f4abaa0735 (7 weeks ago) • 2021-07-01 12:46:11 -0700
+Engine • revision 241c87ad80
+Tools • Dart 2.13.4
 ## 电子书
 
 - [电子书地址](https://book.flutterchina.club/)
 
 ## TODO
-- 添加拍照功能
-- 添加选择图片功能
-- 添加版本升级功能
-- 添加消息推送功能
-- 添加苹果支付功能
-- 添加安卓端微信支付宝支付功能
+* [ ] 添加拍照功能
+* [ ] 添加选择图片上传功能
+* [ ] 添加版本升级功能
+* [ ] 添加消息推送功能
+* [ ] 添加苹果支付功能
+* [ ] 添加安卓端微信支付宝支付功能
--- lib/chapter11/http.dart
+++ lib/chapter11/http.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HttpTestRoute extends StatefulWidget {
   @override
@@ -9,8 +11,15 @@ class HttpTestRoute extends StatefulWidget {
 }
 
 class _HttpTestRouteState extends State<HttpTestRoute> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
   bool _loading = false;
-  String _text = "";
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,52 +27,68 @@ class _HttpTestRouteState extends State<HttpTestRoute> {
       appBar: AppBar(
         title: Text("flutter.cn"),
       ),
-      body: ConstrainedBox(
-        constraints: BoxConstraints.expand(),
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              ElevatedButton(
-                  child: Text("获取Flutter首页"),
-                  onPressed: _loading
-                      ? null
-                      : () async {
-                          setState(() {
-                            _loading = true;
-                            _text = "正在请求...";
-                          });
-                          try {
-                            //创建一个HttpClient
-                            HttpClient httpClient = new HttpClient();
-                            //打开Http连接
-                            HttpClientRequest request = await httpClient
-                                .getUrl(Uri.parse("https://flutter.cn/"));
-                            //使用iPhone的UA
-                            request.headers.add("user-agent",
-                                "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1");
-                            //等待连接服务器（会将请求信息发送给服务器）
-                            HttpClientResponse response = await request.close();
-                            //读取响应内容
-                            _text =
-                                await response.transform(utf8.decoder).join();
-                            //输出响应头
-                            print(response.headers);
-
-                            //关闭client后，通过该client发起的所有请求都会中止。
-                            httpClient.close();
-                          } catch (e) {
-                            _text = "请求失败：$e";
-                          } finally {
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            FutureBuilder<WebViewController>(
+              future: _controller.future,
+              builder: (BuildContext context,
+                  AsyncSnapshot<WebViewController> controller) {
+                return ElevatedButton(
+                    child: Text("获取Flutter纯文本"),
+                    onPressed: _loading
+                        ? null
+                        : () async {
                             setState(() {
-                              _loading = false;
+                              _loading = true;
                             });
-                          }
-                        }),
-              Container(
-                  width: MediaQuery.of(context).size.width - 50.0,
-                  child: Text(_text.replaceAll(new RegExp(r"\s"), "")))
-            ],
-          ),
+                            try {
+                              //创建一个HttpClient
+                              HttpClient httpClient = new HttpClient();
+                              //打开Http连接
+                              HttpClientRequest request = await httpClient
+                                  .getUrl(Uri.parse("https://flutter.cn/"));
+                              //使用iPhone的UA
+                              request.headers.add("user-agent",
+                                  "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1");
+                              //等待连接服务器（会将请求信息发送给服务器）
+                              HttpClientResponse response =
+                                  await request.close();
+                              //读取响应内容
+                              var _text =
+                                  await response.transform(utf8.decoder).join();
+                              var body = base64Encode(
+                                  const Utf8Encoder().convert(_text));
+                              //输出响应头
+                              // print(response.headers);
+                              controller.data
+                                  ?.loadUrl('data:text/html;base64,$body');
+                              controller.data?.evaluateJavascript("alert('Flutter实战');");
+                              //关闭client后，通过该client发起的所有请求都会中止。
+                              httpClient.close();
+                            } catch (e) {
+                              print("请求失败：$e");
+                            } finally {
+                              setState(() {
+                                _loading = false;
+                              });
+                            }
+                          });
+              },
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height - 160.0,
+              width: MediaQuery.of(context).size.width - 20.0,
+              child: WebView(
+                initialUrl: 'https://flutter.cn/',
+                javascriptMode: JavascriptMode.unrestricted,
+                onWebViewCreated: (WebViewController webViewController) {
+                  _controller.complete(webViewController);
+                },
+              ),
+            )
+          ],
         ),
       ),
     );
