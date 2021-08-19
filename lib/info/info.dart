0299b9a156b83b9fbd078ca80213d43ac43ef4e8
wangdengwu <dengwu.wang@gmail.com>
2021-08-19 17:24:12 +0800

    package_info_plus

--- lib/info/info.dart
+++ lib/info/info.dart
@@ -1,14 +1,59 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
 
-class Info extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
+
+class Info extends StatefulWidget {
   const Info({Key? key}) : super(key: key);
 
   @override
+  _InfoState createState() => _InfoState();
+}
+
+class _InfoState extends State<Info> {
+  PackageInfo _packageInfo = PackageInfo(
+    appName: 'Unknown',
+    packageName: 'Unknown',
+    version: 'Unknown',
+    buildNumber: 'Unknown',
+    buildSignature: 'Unknown',
+  );
+  @override
+  void initState() {
+    super.initState();
+    _initPackageInfo();
+  }
+
+  Future<void> _initPackageInfo() async {
+    final info = await PackageInfo.fromPlatform();
+    setState(() {
+      _packageInfo = info;
+    });
+  }
+
+  Widget _infoTile(String title, String subtitle) {
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(subtitle.isEmpty ? 'Not set' : subtitle),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("info"),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          _infoTile('App name', _packageInfo.appName),
+          _infoTile('Package name', _packageInfo.packageName),
+          _infoTile('App version', _packageInfo.version),
+          _infoTile('Build number', _packageInfo.buildNumber),
+          _infoTile('Build signature', _packageInfo.buildSignature),
+        ],
       ),
     );
   }
 }
+
+
